1245440b8369f617ed48b46aa186e9b2dc688d6f
leathan <1@leathan.xyz>
2017-07-28 10:43:49 -0400

    new docs

--- enQue.js
+++ enQue.js
@@ -3,38 +3,46 @@
 // License MIT
 'use strict'
 
+// Construct a new enQue object
 function enQue(init) {
+  // Default to an empty array
   this.que = [];
+  // **if** passed a parameter assume its an array
+  // and add use the add method to add all elements.
   if(init) this.add(init);
 }
 
-enQue.prototype.fill = function(fn, times) {
-  for(let i = 0; i < times; i++) {
+// Fill an enQue object with n functions.
+enQue.prototype.fill = function(fn, n) {
+  for(let i = 0; i < n; i++) {
     this.que.push(fn);
   }
-  return times;
+  // assume no problems and return n.
+  return n;
 }
 
 enQue.prototype.add = function(fn) {
   // If argument is array loop through it
   // adding every item to our que.
   if(fn.constructor.name === 'Array') {
-    let i = 0;
-    for(i = 0; i < fn.length; i++) {
+    let l = fn.length
+    for(let i = 0; i < l; i++) {
       this.que.push(fn)
     }
-    return i;
+    return l;
   }
-  return this.que.push(fn);
+  // returns the amount of added fns.
+  return this.que.push(fn) ? 1 : 0;
 }
 
+// clears all function elements fromt he array.
 enQue.prototype.clear = function(fn) {
   return this.que = [];
 }
 
-// item can be a string, function ref, or number.
-// amount is the amount of items to remove
-// if its not specified all items found are removed.
+// `item` can be a `String`, `Function ref`, or `Number`.
+// `amount` is the amount of found `item`'s to remove
+// if its not specified **all** `item`s found are removed.
 enQue.prototype.remove = function(item, amount) {
   type = item.constructor.name;
   // If item is a number just remove that element and return.
@@ -42,20 +50,21 @@ enQue.prototype.remove = function(item, amount) {
     return this.que.splice(item, 1);
   }
   else {
-    // Otherwise loop through the que and check each element
-    // returns array of removed functions.
+    // Otherwise check if its a `Function` or an `String`
+    // and loop through the que checking each element
     type === 'Function' ? check = item : check = item.toString();
     total = amount || Infinity;
     result = [];
     removed = 0;
     for(i=0; i<this.que.length; i++) {
-      // Done remove more than amount!
+      // Make sure we dont remove more than amount!
       if(total > amount) break;
       if(check === item) {
         result.push(this.que.splice(i, 1));
         removed++;
       }
     }
+    // returns the array of removed elements
     return result;
   }
 }
@@ -65,16 +74,16 @@ enQue.prototype.executeQue = function(data, done) {
   if(!data) data = {};
   // preserve the original callback for potential que rebuilding.
   var orig = done;
-  // i is our iterator, quit/inject check if we need to quit/inject Promise.
+  // `i` is our iterator, quit/inject check if we need to quit/inject `Promise`.
   var i = 0, quit = false, inject = false;
 
   // Perform nesting.
   this.que.reduceRight((done, next) =>
     // Options can be any operation to perform while nesting callbacks.
-    // Currently options must be a specific Object, or a Number, if Object
-    // then it needs a quit or inject property. Otherwise 0 terminates que,
+    // Currently options must be a specific `JSON Object`, or a `Number`, if its JSON
+    // then it needs a `quit` or `inject` property. Otherwise **0** terminates que,
     // exposing the data immitiadtly. A negative Number sends the data
-    // to backwards in the que (to a new que techincally), Positive Numbers
+    // to backwards in the que (to a new que **techincally**), Positive Numbers
     // send the data forward, bellow var i is current callback index being nested.
     options => {
       // check if options is object, the first iteration is always data obj, so skip it.
@@ -89,11 +98,11 @@ enQue.prototype.executeQue = function(data, done) {
       else if(options !== data && options) {
         // create temp que to hold our new que.
         tmpQue = [];
-        // let j be the position we are skipping to.
-        // lets say next(-3) was passed, so options = -3.
-        // i is the current que spot - 1 that called next(-3).
-        // lets say i was 5, so we want to go back to 1.
-        // so j = 5 + -3  - (options < 0) = 1
+        // let `j` be the position we are skipping to.
+        // lets say `next(-3)` was passed, so `options = -3`.
+        // `i` is the current que spot - 1 that called `next(-3)`.
+        // lets say `i` was **5**, so we want to go back to **1**.
+        // so `j = 5 + -3  - (options < 0) = 1`
         // so start at position 1, until the end.
         var j = i + options - (options < 0);
         if(!this.que[j-1]) throw `${options} out of bounds, no que position ${j} exists.`
